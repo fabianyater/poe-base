c9c18be3ff56cdafce86d3adc304603b1088dbfd
Fabian Yate <fabianyate31@gmail.com>
2024-08-05 19:02:47 -0500

    update label builder

--- src/main/utils/Utils.java
+++ src/main/utils/Utils.java
@@ -4,12 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Utils {
+
+    private Utils() {
+    }
+
     public static void centerLabel(JLabel label, Dimension size) {
-        Dimension parentSize = size;
         Dimension labelSize = label.getPreferredSize();
 
-        int x = (parentSize.width - labelSize.width) / 2;
-        int y = (parentSize.height - labelSize.height) / 2;
+        int x = (size.width - labelSize.width) / 2;
+        int y = (size.height - labelSize.height) / 2;
 
         label.setBounds(x, y, labelSize.width, labelSize.height);
     }
