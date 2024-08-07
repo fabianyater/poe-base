55bdb8f6ee0174b6f381710df3a6b82279cf505b
Fabian Yate <fabianyate31@gmail.com>
2024-08-06 23:09:29 -0500

    Add layout property to panel builder

--- src/main/lib/builders/PanelBuilder.java
+++ src/main/lib/builders/PanelBuilder.java
@@ -1,14 +1,26 @@
 package main.lib.builders;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelBuilder extends AbstractComponentBuilder<JPanel> {
+    private LayoutManager layoutManager = null;
+
     public PanelBuilder(String text) {
         super(text);
     }
 
+    public PanelBuilder setLayout(LayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+        return this;
+    }
+
     @Override
     public JPanel build() {
-        return new JPanel();
+        JPanel panel = new JPanel();
+
+        panel.setLayout(layoutManager);
+
+        return panel;
     }
 }
